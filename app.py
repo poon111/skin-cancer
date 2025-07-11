6b8d866d0fba13f9c70141fa7f119ad5517adc21
poon111 <poonnawit10@gmail.com>
2025-07-12 01:47:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from PIL import Image
 st.set_page_config(page_title="Skin Cancer Classification", layout="wide")
 
 # Model download from Google Drive
-MODEL_URL = "https://drive.google.com/uc?id=18bLT5mDOm-I5vVUReN8SldtZS9GJcCWT"
+MODEL_URL = "https://drive.google.com/uc?id=1PcYOD4I_4dtkYTkKOhAOQMJ6ZTVDn__E"
 MODEL_PATH = "best.pt"
 
 @st.cache_resource
